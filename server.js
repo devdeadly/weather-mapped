09f6a9cd2319628b6b007f836d869444b38dd0e8
Colby Thomas <coloradocolby@gmail.com>
2020-08-28 20:02:08 -0700

    google integration fixes

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || '8000';
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || "8000";
 const app = express();
-const axios = require('axios');
+const axios = require("axios");
 
-const GOOGLE_API_KEY = 'AIzaSyC9NoROpZXAgKMhlhIfTspvyZrjuuNS9Bg';
-const DARKSKY_API_KEY = 'b85a054525a7d6b3665d54f019939acb';
-const DARK_SKY_URL = 'https://api.darksky.net/forecast';
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+const DARK_SKY_URL = "https://api.darksky.net/forecast";
 
 // Parsers for POST data
 app.use(bodyParser.json());
@@ -18,35 +17,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Add headers
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
   // Pass to next layer of middleware
   next();
 });
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, "public")));
 
-app.get('/api/weather/:LAT,:LNG', (req, res) => {
-  const { LAT, LNG } = req.params;
-  axios.get(`${DARK_SKY_URL}/${DARKSKY_API_KEY}/${LAT},${LNG}`)
-    .then((resp) => {
-      res.json(resp.data);
-    });
+app.get("/api/weather/:lat,:lng", (req, res) => {
+  const { lat, lng } = req.params;
+  axios.get(`${DARK_SKY_URL}/${DARKSKY_API_KEY}/${lat},${lng}`).then((resp) => {
+    res.json(resp.data);
+  });
 });
 
-app.post(`/api/location/`, (req, res) => {
+app.post(`/api/location`, async (req, res) => {
   const { location } = req.body;
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_API_KEY}&address=${location}`)
-    .then(resp => {
-      res.json(resp.data.results[0])
-    });
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_API_KEY}&address=${location}`
+  );
+  res.json(response.data.results[0]);
 });
 
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on ${PORT} ✨`));
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import moment from 'moment-timezone';
-import ReactAnimatedWeather from 'react-animated-weather';
+import moment from "moment-timezone";
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
 
 const getIcon = (str) => str.replace(/-/g, "_").toUpperCase();
 
 export default (props) => {
   return (
     <div>
-      {
-        props.daily && 
+      {props.daily && (
         <div>
           <table className="table is-narrow">
             <thead>
@@ -21,38 +20,30 @@ export default (props) => {
               </tr>
             </thead>
             <tbody>
-              {
-                props.daily.data.map((day, i) => {
-                  return (
-                    <tr key={i}>
-                      <td>
-                        { moment
-                            .unix(day.time)
-                            .tz(props.timezone)
-                            .format('dddd') }
-                      </td>
-                      <td>
-                        <ReactAnimatedWeather
-                          icon={getIcon(day.icon)}
-                          color={'#ddd'}
-                          size={25}
-                          animate={true}
-                        />
-                      </td>
-                      <td>{Math.round(day.temperatureLow)}</td>
-                      <td>{Math.round(day.temperatureHigh)}</td>
-                      <td className="summary">{day.summary.slice(0, -1)}</td>
-                    </tr>
-                  )
-                })
-              }
+              {props.daily.data.map((day, i) => {
+                return (
+                  <tr key={i}>
+                    <td>
+                      {moment.unix(day.time).tz(props.timezone).format("dddd")}
+                    </td>
+                    <td>
+                      <ReactAnimatedWeather
+                        icon={getIcon(day.icon)}
+                        color={"#ddd"}
+                        size={25}
+                        animate={true}
+                      />
+                    </td>
+                    <td>{Math.round(day.temperatureLow)}</td>
+                    <td>{Math.round(day.temperatureHigh)}</td>
+                    <td className="summary">{day.summary.slice(0, -1)}</td>
+                  </tr>
+                );
+              })}
             </tbody>
-          </table>   
-        </div>       
-      }
+          </table>
+        </div>
+      )}
     </div>
   );
-}
-// <button className="button is-link" onClick={() => console.log(props)}>Table Props</button>
-
-
+};
