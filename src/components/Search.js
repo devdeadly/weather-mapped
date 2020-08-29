cb01c1ca0e0318bdbca66b67a499be00994da26e
Colby Thomas <coloradocolby@gmail.com>
2020-08-28 20:39:09 -0700

    google integration fixes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,24 @@
-import React, {Component} from 'react';
-
+import React from "react";
 
 export default (props) => {
-  
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
   const handleChange = (e) => {
     e.persist();
     props.getCoordinates(e.target.value);
-  }
+  };
 
   return (
     <div className="header">
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          placeholder="search for weather by location..."
+        <input
+          type="text"
+          placeholder="search weather by location..."
           onChange={handleChange}
           autoFocus
         />
       </form>
     </div>
   );
-}
+};
